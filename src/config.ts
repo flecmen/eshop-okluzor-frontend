81e71f1ceeda9fb14cdfe38ab4cc0b09071c5723
David Flek <davidovkyflekovky@gmail.com>
2023-02-09 16:32:13 +0100

    update backend url

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
 export default {
-  backendUrl: 'http://localhost:3000',
+  backendUrl: 'https://eshop-okluzor-backend-production.up.railway.app',
   regex_email: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 }
