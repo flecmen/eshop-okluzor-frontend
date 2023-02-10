b2974a52d0c7a9f6d80d6ec7a56f0bbe1227d7bb
David Flek <davidovkyflekovky@gmail.com>
2023-02-10 03:03:06 +0100

    lepší čitelnost kodu + nefunkční search button pro filtr

--- src/components/tables/customersTable.vue
+++ src/components/tables/customersTable.vue
@@ -5,13 +5,12 @@
     @close-modal="revertUserModal"
   ></userModal>
   <div class="q-pa-md">
-    <error-banner />
-    <successBanner />
-
+    <q-btn label="Nový" color="positive"></q-btn>
     <q-table
       title="Zákazníci"
-      :rows="rows"
-      :columns="columns"
+      :loading="table.isLoading"
+      :rows="table.rows"
+      :columns="table.columns"
       row-key="name"
       :visible-columns="[
         'edit',
@@ -25,6 +24,30 @@
         'orders',
       ]"
     >
+      <template v-slot:top-right>
+        <div class="q-gutter-md row items-start">
+          <q-select
+            filled
+            v-model="table.searchCol"
+            :options="table.searchOptions"
+            label="Sloupec"
+            dense
+            options-dense
+            clearable
+          />
+          <q-input
+            borderless
+            dense
+            debounce="300"
+            v-model="table.searchfilter"
+            placeholder="Search"
+          >
+            <template v-slot:append>
+              <q-icon name="search" />
+            </template>
+          </q-input>
+        </div>
+      </template>
       <template v-slot:body-cell-edit="props">
         <q-td :props="props" class="justify-center">
           <q-btn
@@ -67,80 +90,85 @@ import { ref, onMounted, reactive } from 'vue';
 import { User } from '../../types/dbTypes';
 import { useUserStore } from 'src/stores/user-store';
 import { useAdminStore } from 'src/stores/admin-store';
-import errorBanner from '../banners/errorBanner.vue';
-import successBanner from '../banners/successBanner.vue';
 import userModal from '../modals/userModal.vue';
+
 function funkce(props) {
   console.log(props);
 }
 const userStore = useUserStore();
 const adminStore = useAdminStore();
-const columns = ref([
-  // array of Objects
-  // column Object definition
-  {
-    name: 'edit',
-    label: '',
-    field: '',
-    align: 'left',
-  },
-  {
-    name: 'id',
-    field: 'id',
-  },
-  {
-    // unique id
-    // identifies column
-    // (used by pagination.sortBy, "body-cell-[name]" slot, ...)
-    name: 'nazev_firmy',
+const table = reactive({
+  columns: [
+    // array of Objects
+    // column Object definition
+    {
+      name: 'edit',
+      label: '',
+      field: '',
+      align: 'left',
+    },
+    {
+      name: 'id',
+      field: 'id',
+    },
+    {
+      // unique id
+      // identifies column
+      // (used by pagination.sortBy, "body-cell-[name]" slot, ...)
+      name: 'nazev_firmy',
 
-    // label for header
-    label: 'Firma',
+      // label for header
+      label: 'Firma',
 
-    // row Object property to determine value for this column
-    field: 'nazev_firmy',
-    // OR field: row => row.some.nested.prop,
+      // row Object property to determine value for this column
+      field: 'nazev_firmy',
+      // OR field: row => row.some.nested.prop,
 
-    // (optional) if we use visible-columns, this col will always be visible
-    required: true,
+      // (optional) if we use visible-columns, this col will always be visible
+      required: true,
 
-    // (optional) alignment
-    align: 'left',
-    format: (val: string) => `${val}`,
-    sortOrder: 'ad', // or 'da'
-    headerClasses: 'my-special-class',
-  },
-  { name: 'ico', label: 'ičo', field: 'ico', align: 'left' },
-  { name: 'dic', label: 'dič', field: 'dic', align: 'left' },
-  { name: 'tel', label: 'tel', field: 'tel', align: 'left' },
-  { name: 'email', label: 'email', field: 'email', align: 'left' },
-  {
-    name: 'address',
-    label: 'adresa',
-    align: 'left',
-    field: (row: User) =>
-      `${row.address.mesto}, ${row.address.ulice} ${row.address.cislo_popis}/${row.address.cislo_orient}, ${row.address.psc}`,
-    sortable: true,
-  },
-  {
-    name: 'branches',
-    label: 'pobočky',
-    field: '',
-    align: 'left',
-  },
-  {
-    name: 'orders',
-    label: 'objednávky',
-    field: '',
-    align: 'left',
-  },
-]);
-const rows = ref<typeof adminStore.users>([] as typeof adminStore.users);
+      // (optional) alignment
+      align: 'left',
+      format: (val: string) => `${val}`,
+      sortOrder: 'ad', // or 'da'
+      headerClasses: 'my-special-class',
+    },
+    { name: 'ico', label: 'ičo', field: 'ico', align: 'left' },
+    { name: 'dic', label: 'dič', field: 'dic', align: 'left' },
+    { name: 'tel', label: 'tel', field: 'tel', align: 'left' },
+    { name: 'email', label: 'email', field: 'email', align: 'left' },
+    {
+      name: 'address',
+      label: 'adresa',
+      align: 'left',
+      field: (row: User) =>
+        `${row.address.mesto}, ${row.address.ulice} ${row.address.cislo_popis}/${row.address.cislo_orient}, ${row.address.psc}`,
+      sortable: true,
+    },
+    {
+      name: 'branches',
+      label: 'pobočky',
+      field: '',
+      align: 'left',
+    },
+    {
+      name: 'orders',
+      label: 'objednávky',
+      field: '',
+      align: 'left',
+    },
+  ],
+  rows: [] as typeof adminStore.users,
+  isLoading: true,
+  searchOptions: ['Firma', 'email'],
+  searchCol: '',
+  searchfilter: '',
+});
 
 onMounted(async () => {
   await adminStore.loadUsers();
-  rows.value = ref(adminStore.users).value;
-  console.log(rows.value);
+  table.rows = adminStore.users;
+  table.isLoading = false;
 });
 
 const isUserModalVisible = ref(false);
