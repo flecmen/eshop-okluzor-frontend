0797ccd9f6051f5429a4e55eabe89579cb725edc
David Flek <davidovkyflekovky@gmail.com>
2023-02-28 19:44:18 +0100

    env BackendUrl

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 import { Address } from './types/dbTypes';
 
 export default {
-  backendUrl: 'http://localhost:3000',
+  backendUrl: process.env['BackendUrl'],
   regex_email: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
   formatAddress: (address: Address) => {
     // use template literals to construct formatted string
